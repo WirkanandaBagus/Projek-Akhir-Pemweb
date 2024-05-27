8d010cd6bb1312a28eea607f6e6ed501fa1bb253
poll44 <faiz.ihsan.akram@gmail.com>
2024-05-27 20:34:59 +0700

    tambah dikit
    apus sqlite
    benerin header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,49 @@
 <nav class="navbar">
     <img class="logo" src="{{URL::to('img/logo.png')}}" />
-    <li class="div-nav">
-    <a class="{{$title === 'Login' ? 'menus' : 'menu'}}" href="/login">Login</a>
-        <a class="{{$title === 'Home' ? 'menus' : 'menu'}}" href="/">Home</a>
-        <a class="{{$title === 'Division' ? 'menus' : 'menu'}}" href="/division">Division</a>
-        <a class="{{$title === 'Relation' ? 'menus' : 'menu'}}" href="/relation">Relation</a>
-        <a class="{{$title === 'Publication' ? 'menus' : 'menu'}}" href="/publication">Publication</a>
-        <a class="{{$title === 'Service' ? 'menus' : 'menu'}}" href="/service">Service</a>
-    </li>
+    <ul class="div-nav">
+        <li>
+            <a class="{{$title === 'Login' ? 'menus' : 'menu'}}" href="/login">Login</a>
+        </li>
+        <li>
+            <a class="{{$title === 'Home' ? 'menus' : 'menu'}}" href="/">Home</a>
+        </li>
+        <li class="menu {{$title === 'Division' ? 'menus' : 'menu'}}">
+            Division <i class="bi bi-caret-right"></i>
+            <div class="dropdown-content">
+                <div class="dropdown-item">
+                    <a href="/bpi">BPI</a>
+                </div>
+                <div class="dropdown-item">
+                    <a href="/humanoid">Humanoid</a>
+                </div>
+                <div class="dropdown-item">
+                    <a href="/quadcopter">Quadcopter</a>
+                </div>
+                <div class="dropdown-item">
+                    <a href="/amarine">Amarine</a>
+                </div>
+                <div class="dropdown-item">
+                    <a href="/public_relation">Public Relation</a>
+                </div>
+                <div class="dropdown-item">
+                    <a href="/comp.management">Comp. Management</a>
+                </div>
+                <div class="dropdown-item">
+                    <a href="/creative-media">Creative Media</a>
+                </div>
+                <div class="dropdown-item">
+                    <a href="/fund-raising">Fund Raising</a>
+                </div>
+            </div>
+        </li>
+        <li>
+            <a class="{{$title === 'Relation' ? 'menus' : 'menu'}}" href="/relation">Relation</a>
+        </li>
+        <li>
+            <a class="{{$title === 'Publication' ? 'menus' : 'menu'}}" href="/publication">Publication</a>
+        </li>
+        <li>
+            <a class="{{$title === 'Service' ? 'menus' : 'menu'}}" href="/service">Service</a>
+        </li>
+    </ul>
 </nav>
